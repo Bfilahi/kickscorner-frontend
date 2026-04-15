39b34c5a5fad8aa86a736eaccf95c1c06768417b
Bfilahi <abdelbassete.filahi@gmail.com>
2026-04-16 00:29:59 +0200

    Tested service: hero-service

--- src/app/services/home/hero-service.spec.ts
+++ src/app/services/home/hero-service.spec.ts
@@ -1,5 +1,8 @@
 import { TestBed } from '@angular/core/testing';
+import 'zone.js';
+
 import { HeroService } from './hero-service';
+
 
 describe('HeroService', () => {
   let service: HeroService;
@@ -12,4 +15,22 @@ describe('HeroService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should initialize GLTFLoader', () => {
+    expect(service['gltfLoader']).toBeTruthy();
+  });
+
+  describe('createScene()', () => {
+    it('should create scene and camera', () => {
+      const canvas = document.createElement('canvas');
+
+      service.createScene({nativeElement: canvas});
+
+      expect(service['scene']).toBeTruthy();
+      expect(service['camera']).toBeTruthy();
+      expect(service['renderer']).toBeTruthy();
+      expect(service['renderer'].shadowMap.enabled).toBeTrue();
+    });
+  });
+
 });
