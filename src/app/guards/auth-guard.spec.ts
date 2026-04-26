2eac240fd6f95afadf9b7b908918b8e92b77a831
Bfilahi <abdelbassete.filahi@gmail.com>
2026-04-27 00:02:00 +0200

    Tested guard: auth-guard

--- src/app/guards/auth-guard.spec.ts
+++ src/app/guards/auth-guard.spec.ts
@@ -1,17 +1,61 @@
 import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot } from '@angular/router';
 
 import { authGuard } from './auth-guard';
+import { AuthService } from '../services/auth/auth-service';
+
 
 describe('authGuard', () => {
   const executeGuard: CanActivateFn = (...guardParameters) => 
       TestBed.runInInjectionContext(() => authGuard(...guardParameters));
 
+  let mockAuthService: jasmine.SpyObj<AuthService>;
+  let router: Router;
+
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    mockAuthService = jasmine.createSpyObj(['isLoggedIn']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        {provide: AuthService, useValue: mockAuthService}
+      ]
+    });
+
+    router = TestBed.inject(Router);
   });
 
   it('should be created', () => {
     expect(executeGuard).toBeTruthy();
   });
+
+  it('should return true when the user is authenticated', () => {
+    mockAuthService.isLoggedIn.and.returnValue(true);
+
+    const result = executeGuard({} as ActivatedRouteSnapshot, {} as RouterStateSnapshot);
+    expect(result).toBeTrue();
+  });
+
+  it('should not trigger the navigation when user is authenticated', () => {
+    mockAuthService.isLoggedIn.and.returnValue(true);
+    spyOn(router, 'navigate');
+
+    executeGuard({} as ActivatedRouteSnapshot, {} as RouterStateSnapshot);
+    expect(router.navigate).not.toHaveBeenCalled();
+  });
+
+  it('should return false when the user is not authenticated', () => {
+    mockAuthService.isLoggedIn.and.returnValue(false);
+
+    const result = executeGuard({} as ActivatedRouteSnapshot, {} as RouterStateSnapshot);
+    expect(result).toBeFalse();
+  });
+
+  it('should call router.navigate with /login', () => {
+    mockAuthService.isLoggedIn.and.returnValue(false);
+    spyOn(router, 'navigate');
+
+    executeGuard({} as ActivatedRouteSnapshot, {} as RouterStateSnapshot);
+    expect(router.navigate).toHaveBeenCalledWith(['/login']);
+  });
+
 });
